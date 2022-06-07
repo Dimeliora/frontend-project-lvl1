47e2cad350803c39bf7660f9f1c65389e290db47
Dimeliora <dimeliora710@gmail.com>
2022-06-07 21:54:32 +0300

    [Step-4] Add eslint package with airbnb config, add eslint & prettier rule files, add CodeClimate link into README.md, lint files

--- src/cli.js
+++ src/cli.js
@@ -1,10 +1,10 @@
-import readlineSync from "readline-sync";
+import readlineSync from 'readline-sync';
 
 const greetUser = () => {
-  console.log("Welcome to the Brain Games!");
+  console.log('Welcome to the Brain Games!');
 
-  const userName = readlineSync.question("May I have your name? ");
+  const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}`);
 };
 
-export { greetUser };
+export default greetUser;
